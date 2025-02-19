f3a027432590f4e43706c43e90d3d1dea9af1271
EmilyPoltorak <EmilyPoltorak@localhost>
2025-02-19 14:21:46 -0600

    Studio 4 complete

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,20 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setCanvasSize(700, 700);
+		StdDraw.setXscale(0, 700);
+		StdDraw.setYscale(0, 700);
+    		
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.rectangle(350, 350, 300, 200);
+		
+		StdDraw.setPenColor(StdDraw.BLUE);
+		
+		StdDraw.filledRectangle(225, 200, 175, 50);
+		StdDraw.filledRectangle(150, 300, 100, 50);
+		StdDraw.filledRectangle(225, 400, 175, 50);
+		StdDraw.filledRectangle(150, 500, 100, 50);
+		
+		
 	}
 }
